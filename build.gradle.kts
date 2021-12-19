f880c42551e9ec9a494af7009eefa5d616db6609
DK09 <2270816244@qq.com>
2021-12-19 15:32:11 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    val kotlinVersion = "1.4.30"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+
+    id("net.mamoe.mirai-console") version "2.6.6"
+}
+
+group = "org.example"
+version = "0.1.0"
+
+repositories {
+    mavenLocal()
+    maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
+    mavenCentral()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
+rootProject.name = "mirai-console-plugin-template"
